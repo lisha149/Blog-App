e7a6b0262d3afa4b83046c3aa742c0eb6e17b655
palisha shakya <palishashakya@gmail.com>
2022-07-07 09:49:22 +0545

    list of articles used as reusable component

--- frontend/src/pages/Article.js
+++ frontend/src/pages/Article.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import articleContent from "../content/ArticleContent";
+import Articles from "../components/Articles";
 
 const Article = () => {
   const { name } = useParams();
   const article = articleContent.find((article) => article.name === name);
   if (!article) return <h1>Article doesn't exist</h1>;
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
   return (
     <>
       <h1 className="sm:text-3xl text-xl font-bold my-6 text-gray-900">
@@ -14,6 +18,12 @@ const Article = () => {
       <p className="mx-auto leading-loose text-base text-justify mb-4">
         {article.content}
       </p>
+      <h1 className="sm:text-2x text-xl font-bold my-4 text-gray-900">
+        Other Articles
+      </h1>
+      <div className="flex flex-wrap -m-4">
+        <Articles articles={otherArticles} />
+      </div>
     </>
   );
 };
