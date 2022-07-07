27d7104262b5af9dc61c4f4e8e86f04054319f99
palisha shakya <palishashakya@gmail.com>
2022-07-07 12:07:41 +0545

    Added the comments in an article

--- backend/server.js
+++ backend/server.js
@@ -8,28 +8,51 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/article/:name", async (req, res) => {
+const connectDB = async (operations, res) => {
   try {
-    const articleName = req.params.name;
     const client = await MongoClient.connect("mongodb://localhost:27017", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     const db = client.db("blog");
-    const articleDetails = await db
-      .collection("articles")
-      .findOne({ name: articleName });
-    res.status(200).json(articleDetails);
+    await operations(db);
     client.close();
   } catch (error) {
     res.status(500).json({ message: "Error connecting to db", error });
   }
+};
+
+app.get("/api/article/:name", async (req, res) => {
+  connectDB(async (db) => {
+    const articleName = req.params.name;
+
+    const articleDetails = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articleDetails);
+  }, res);
 });
 
 app.post("/api/article/:name/post-comments", (req, res) => {
-  const { username, text } = req.body;
-  const articleName = req.params.name;
-  articleDetails[articleName].comments.push({ username, text });
-  res.status(200).send(articleDetails[articleName]);
+  connectDB(async (db) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+    const articleDetails = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          comments: articleDetails.comments.concat({ username, text }),
+        },
+      }
+    );
+    const updatedArticleDetails = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(updatedArticleDetails);
+  }, res);
 });
+
 app.listen(5000, console.log("Server started on PORT 5000"));
