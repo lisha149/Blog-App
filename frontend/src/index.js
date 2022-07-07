3722a5216e3d0dd0adc6ed3008447182b68450b0
palisha shakya <palishashakya@gmail.com>
2022-07-07 12:22:29 +0545

    making web requests in the browser

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
+import "whatwg-fetch";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
 import "./styles/tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
