b4222aa22678fd79aaa1197d0456bf34b146c791
palisha shakya <palishashakya@gmail.com>
2022-07-09 12:58:27 +0545

    added an article

--- frontend/src/content/ArticleContent.js
+++ frontend/src/content/ArticleContent.js
@@ -4,7 +4,7 @@ const articleContent = [
     title: "Yomari- Ethnic Newari Dish ",
     thumbnail: "/assets/yomari.jpg",
     content:
-      "Yomari mix- In a bowl, mix rice flour and all purpose flour. Add warm water little at a time and start kneading. Finished dough should be soft and separate easily when pulled. Once the dough is ready, keep it aside for about 10 minutes.Chaku - In a high temperature, heat up a deep cooking pot.Put the jaggery balls and start stiring. Once all melted, add 2 spoons of ghee and stir it again. Then, add little milk and stir again for a minute. Take it out in bowl, add grated dry coconut and brown sesame seeds.Yomari cover- Take a little bit of oil in one hand and take a small portion of the dough. Make a round and then make a cone shape. Dip your finger in oil and make a hole on the top of the dough. Press down to make a bigger hole. Rotate the cover while you make the hole. Fill the cover with the chaku . (If the chaku gets too thick, microwave for 20 seconds then pour it) Fill it up to halfway and close the top. Cook the Yomari in the steamer for about 15 minutes.",
+      "Yomari mix- In a bowl, mix rice flour and all purpose flour. Add warm water little at a time and start kneading. Finished dough should be soft and separate easily when pulled. Once the dough is ready, keep it aside for about 10 minutes. Chaku - In a high temperature, heat up a deep cooking pot.Put the jaggery balls and start stiring. Once all melted, add 2 spoons of ghee and stir it again. Then, add little milk and stir again for a minute. Take it out in bowl, add grated dry coconut and brown sesame seeds. Yomari cover- Take a little bit of oil in one hand and take a small portion of the dough. Make a round and then make a cone shape. Dip your finger in oil and make a hole on the top of the dough. Press down to make a bigger hole. Rotate the cover while you make the hole. Fill the cover with the chaku . (If the chaku gets too thick, microwave for 20 seconds then pour it) Fill it up to halfway and close the top. Cook the Yomari in the steamer for about 15 minutes.",
   },
 
   {
@@ -13,6 +13,13 @@ const articleContent = [
     thumbnail: "/assets/Chatamari.jpg",
     content:
       "Take a mixing bowl with rice flour. Slowly add water and prepare a thin batter.Take chicken keema in a separate bowl. Add salt, chili powder, cumin powder and ginger garlic paste and mix thoroughly.Heat a non - stick frying pan.Pour half a table - spoon of oil.Take the rice flour batter in a ladle and pour in the frying pan in the shape of a circle.And cook for 3 seconds.Add buff / chicken keema on top of the pancake type base.Spread and level the keema while leaving a space in the center.Cook for around 10 minutes with lid on.Carefully crack egg in the central space. Sprinkle some chopped coriander leaves and add sliced tomatoes. Cook for around 3-4 minutes with lid on.",
+  },
+  {
+    name: "Chicken-Alfredo",
+    title: "Chicken Alfredo- The Classic Pasta ",
+    thumbnail: "/assets/alfredo.jpg",
+    content:
+      "In a large skillet over medium-high heat, heat oil. Add chicken and season with salt and pepper. Cook until golden and cooked through, 8 minutes per side. Let rest 10 minutes, then slice. Add milk, broth, and garlic to skillet. Season with salt and pepper and bring to a simmer. Add fettuccine, stirring frequently for about 3 minutes. Let cook until al dente, 8 minutes more. Stir in heavy cream and Parmesan until combined. Simmer until sauce thickens. Remove from heat and stir in sliced chicken. Garnish with parsley. ",
   },
 ];
 export default articleContent;
